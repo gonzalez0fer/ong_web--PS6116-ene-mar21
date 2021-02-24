14d04dbafbf55396f5e5ee72402bff17f1ada215
Javier <javivasv@hotmail.com>
2021-02-24 18:26:10 -0400

    Śeccion del sidebar de Agua se mantiene encendido en todo el modulo

--- apps/dashboard/water_managements/urls.py
+++ apps/dashboard/water_managements/urls.py
@@ -6,17 +6,17 @@ app_name = "water_managements"
 
 urlpatterns = [
     path(
-        '<int:pk>', 
+        '<int:pk>/', 
         views.WaterManagementList.as_view(), 
         name='water_management_list'
         ),
     path(
-        '<int:tank_id>/create', 
+        '<int:tank_id>/create/', 
         views.WaterManagementCreateView.as_view(), 
         name='water_management_create'
         ),
     path(
-        '<int:tank_id>/<int:pk>/update', 
+        '<int:tank_id>/<int:pk>/update/', 
         views.WaterManagementUpdateView.as_view(), 
         name='water_management_update'
         ),             
