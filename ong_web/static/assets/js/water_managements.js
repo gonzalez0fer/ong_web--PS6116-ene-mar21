abc8e0e43ed0e3f2971a36c3d79504a6fc08ef36
Javier <javivasv@hotmail.com>
2021-02-25 14:30:40 -0400

    Campo Descripcion en update. Validaciones dinamicas. Ajuste de validaciones

--- ong_web/static/assets/js/water_managements.js
+++ ong_web/static/assets/js/water_managements.js
@@ -1,8 +1,18 @@
 document.addEventListener('DOMContentLoaded', function() {
+    /* water_managements-create */
     document.getElementById("operation_type").addEventListener("change", type_validation)
     document.getElementById("operation_description").addEventListener("change", description_validation)
-    document.getElementById("water_liters").addEventListener("change", liters_validation)
+
+    if (window.location.href.includes("create")) {
+        document.getElementById("water_liters").addEventListener("change", liters_validation)
+    }
+    
     document.getElementById("water_amount").addEventListener("change", amount_validation)
+
+    /* water_managements-update */
+    if (window.location.href.includes("update")) {
+        document.getElementById("water_liters_update").addEventListener("change", liters_update_validation)
+    }
 })
 
 
@@ -12,7 +22,6 @@ function type_validation() {
     if (operation_type == "none") {
         document.getElementById("operation_type_error").innerHTML = "Debe seleccionar una opción válida"
         document.getElementById("operation_type_error").style.display = "block"
-        error = true
     }
     else {
         document.getElementById("operation_type_error").style.display = "none"
@@ -26,7 +35,6 @@ function description_validation() {
     if (operation_description == "none") {
         document.getElementById("operation_description_error").innerHTML = "Debe seleccionar una opción válida"
         document.getElementById("operation_description_error").style.display = "block"
-        error = true
     }
     else {
         document.getElementById("operation_description_error").style.display = "none"
@@ -40,7 +48,6 @@ function liters_validation() {
     if (parseInt(water_liters) < 1) {
         document.getElementById("water_liters_error").innerHTML = "Debe introducir una cantidad de litros mayor a 0"
         document.getElementById("water_liters_error").style.display = "block"
-        error = true
     }
     else {
         document.getElementById("water_liters_error").style.display = "none"
@@ -54,7 +61,6 @@ function liters_validation() {
         if (tank_current_liters + parseInt(water_liters) > tank_capacity) {
             document.getElementById("water_liters_error").innerHTML = "Debe ingresar una cantidad válida para la capacidad del tanque"
             document.getElementById("water_liters_error").style.display = "block"
-            error = true
         }
     }
 
@@ -62,7 +68,6 @@ function liters_validation() {
         if (parseInt(water_liters) > tank_current_liters) {
             document.getElementById("water_liters_error").innerHTML = "No puede extraer más litros de la cantidad actual del tanque"
             document.getElementById("water_liters_error").style.display = "block"
-            error = true
         }
     }
 }
@@ -74,7 +79,6 @@ function amount_validation() {
     if (parseInt(water_amount) < 0) {
         document.getElementById("water_amount_error").innerHTML = "Debe introducir un precio igual o mayor a 0"
         document.getElementById("water_amount_error").style.display = "block"
-        error = true
     }
     else {
         document.getElementById("water_amount_error").style.display = "none"
@@ -87,7 +91,15 @@ function validate() {
 
     operation_type = document.getElementById("operation_type").value
     operation_description = document.getElementById("operation_description").value
-    water_liters = document.getElementById("water_liters").value
+
+    if (window.location.href.includes("create")) {
+        water_liters = document.getElementById("water_liters").value
+    }
+
+    if (window.location.href.includes("update")) {
+        water_liters = document.getElementById("water_liters_update").value
+    }
+
     water_amount = document.getElementById("water_amount").value
 
     if (operation_type == "none") {
@@ -119,5 +131,38 @@ function validate() {
     }
     else {
         return true
+    }
+}
+
+
+function liters_update_validation() {
+    water_liters = document.getElementById("water_liters_update").value
+    
+    if (parseInt(water_liters) < 1) {
+        document.getElementById("water_liters_error").innerHTML = "Debe introducir una cantidad de litros mayor a 0"
+        document.getElementById("water_liters_error").style.display = "block"
+        error = true
+    }
+    else {
+        document.getElementById("water_liters_error").style.display = "none"
+    }
+
+    operation_type = document.getElementById("operation_type").value
+    tank_capacity = parseInt(tank_capacity)
+    tank_current_liters = parseInt(tank_current_liters)
+    original_quantity = parseInt(original_quantity)
+
+    if (operation_type == "ingreso") {
+        if (tank_current_liters - original_quantity + parseInt(water_liters) > tank_capacity) {
+            document.getElementById("water_liters_error").innerHTML = "Debe ingresar una cantidad válida para la capacidad del tanque"
+            document.getElementById("water_liters_error").style.display = "block"
+        }
+    }
+
+    if (operation_type == "consumo") {
+        if (parseInt(water_liters) > tank_current_liters) {
+            document.getElementById("water_liters_error").innerHTML = "No puede extraer más litros de la cantidad actual del tanque"
+            document.getElementById("water_liters_error").style.display = "block"
+        }
     }
 }
