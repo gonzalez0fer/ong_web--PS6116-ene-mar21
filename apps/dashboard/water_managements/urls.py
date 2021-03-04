00802589e9a025ae3378aa3da9ed75012ac0f8ba
mafer <mafermach@gmai.com>
2021-03-04 17:59:18 -0400

    arreglando cambio de url

--- apps/dashboard/water_managements/urls.py
+++ apps/dashboard/water_managements/urls.py
@@ -6,7 +6,7 @@ app_name = "water_managements"
 
 urlpatterns = [
     path(
-        '<int:pk>/', 
+        '<int:pk>', 
         views.WaterManagementList.as_view(), 
         name='water_management_list'
         ),
@@ -18,7 +18,7 @@ urlpatterns = [
         ),
 
     path(
-        '<int:tank_id>/create/', 
+        '<int:tank_id><int:op_type>/create', 
         views.WaterManagementCreateView.as_view(), 
         name='water_management_create'
         ),
@@ -36,7 +36,7 @@ urlpatterns = [
         ),
 
     path(
-        '<int:tank_id>/<int:pk>/update/', 
+        '<int:tank_id>/<int:pk>/update', 
         views.WaterManagementUpdateView.as_view(), 
         name='water_management_update'
         ),
