06aa53b3f2710fc943469700f1f8419cc39452ed
Pedro Maldonado <pjmg96@gmail.com>
2021-03-06 20:39:11 -0400

    Ajustes modelo mantenimiento y flujos de listar y crear

--- apps/dashboard/maintenance/views.py
+++ apps/dashboard/maintenance/views.py
@@ -1,29 +1,153 @@
 from django.shortcuts import get_object_or_404, redirect, render
 from django.views.generic import ListView
+from django.views.generic.edit import UpdateView, CreateView
 
-#from .forms import RefectoryForm, WaterTankForm, WaterExtraFieldsForm
+from django.utils.decorators import method_decorator
+from django.contrib.auth.decorators import login_required
+from apps.main.users.decorators import superuser_required
+
+from .forms import MaintenanceForm
+from apps.dashboard.maintenance.forms import ProductManagementForm
 from apps.main.maintenance.models import Maintenance
 from apps.main.refectories.models import Refectory
+from apps.main.products.models import Product
 
-class RefectoriesMaintenanceListView(ListView):
-    template_name = "maintenance/refectories_maintenance_list.html"
-    queryset = Refectory.objects.all()
+@method_decorator([login_required, superuser_required], name='dispatch')
+class MaintenanceList(ListView):
+    template_name = ""
+    queryset = Maintenance.objects.all()
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        query = Refectory.objects.order_by('id')
+        
+        refectory = Refectory.objects.get(id=self.kwargs['refectory_id'])
+        query = Maintenance.objects.filter(equipment=self.kwargs['pk']).order_by('created')
         
         context['object_list'] = []
+        context['refectory_data'] = []
 
         for i in query:
-            if len(i.user_asigned.all()) != 0:
-                mandated =''
-                for j in i.user_asigned.all():
-                    mandated = mandated + str(j.name)+' '+str(j.last_name)
-                    if j:
-                        mandated = mandated + ' - '
-            else:
-                mandated = '(por asignar)'
-            context['object_list'].append({'id':i.id, 'name':i.name, 
-            'address':i.address, 'capacity':i.water_tank.capacity,'mandated':mandated })
-        return context  
+            context['object_list'].append({
+                    'id':i.id,
+                    'activity':i.activity,
+                    'comments':i.comments,
+                    'product_name':i.product.product_name,
+                    'product_quantity':i.product_quantity,
+                    'created_by':str(i.created_by.profile.name) + ' ' + str(i.created_by.profile.last_name),
+                    'created':i.created,
+            })
+
+        context['refectory_data'].append({
+            'id':refectory.id,
+            'refectory_name': refectory.name,
+            'refectory_address': refectory.address,
+            })
+        return context
+
+class MaintenanceListGuest(ListView):
+    template_name = ""
+    queryset = Maintenance.objects.all()
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        
+        refectory = Refectory.objects.get(id=self.request.user.profile.refectory.id)
+        query = Maintenance.objects.filter(equipment=self.kwargs['pk']).order_by('created')
+        
+        context['object_list'] = []
+        context['refectory_data'] = []
+
+        for i in query:
+            context['object_list'].append({
+                    'id':i.id,
+                    'activity':i.activity,
+                    'comments':i.comments,
+                    'product_name':i.product.product_name,
+                    'product_quantity':i.product_quantity,
+                    'created_by':str(i.created_by.profile.name) + ' ' + str(i.created_by.profile.last_name),
+                    'created':i.created,
+            })
+
+        context['refectory_data'].append({
+            'id':refectory.id,
+            'refectory_name': refectory.name,
+            'refectory_address': refectory.address,
+            })
+        return context
+
+@method_decorator([login_required, superuser_required], name='dispatch')
+class MaintenanceCreateView(CreateView):
+    model = Maintenance
+    form_class = MaintenanceForm
+    template_name = ""
+    
+    def get_success_url(self):
+        if self.request.user.is_superuser:
+            success_url = ""
+        else:
+            pass
+        return success_url
+
+    def get_context_data(self, **kwargs):
+        context = super(MaintenanceCreateView, self).get_context_data(**kwargs)
+
+        query = Product.objects.filter(refectory_id=self.kwargs['refectory_id']).order_by('product_name')
+        
+        context['product_info'] = []
+
+        for i in query:
+            context['product_info'].append({
+                "product_name": i.product_name,
+                "product_quantity": i.total_product_quantity,
+            })
+
+        context['refectory'] = {
+            'id' : self.kwargs['refectory_id'],
+        }
+
+        return context
+
+    def post(self, request, *args, **kwargs):
+        self.object = None
+        form = self.get_form()
+        product_used = Product.objects.get(refectory_id=self.kwargs['refectory_id'],product_name=form.data['product_name'])
+        product_management_form = ProductManagementForm(
+            initial={
+                'product_cod':product_used,
+                'product_name':product_used.product_name,
+                'product_unit':product_used.product_unit,
+                'operation_type':'Egreso',
+                'product_quantity':form.data['product_quantity'],
+                'product_unitary_amount':0,
+        })
+
+        if form.is_valid() and product_management_form.is_valid():
+            return self.form_valid(form, product_management_form, product_used)
+        else:
+            return self.form_invalid(form, product_management_form)
+
+    def form_valid(self, form, product_management_form, product_used):
+        self.object = form.save(commit=False)
+        self.object.equipment = self.kwargs['pk']
+        self.object.created_by = self.request.user
+        self.object.product = product_used
+        
+
+        product_management_object = product_management_form.save(commit=False)
+        product_management_object.product_total_amount = 0
+        product_management_object.created_by = self.request.user
+        
+        #validacion de cantidad disponible
+        product_used.total_product_quantity -= self.object.product_quantity 
+
+        product_management_object.save()
+        self.object.save()
+        return super().form_valid(form)
+
+    def form_invalid(self, form, product_management_form):
+        return self.render_to_response(
+            self.get_context_data(
+                form=form,
+                product_management_form=product_management_form,
+            )
+        )
--- apps/main/maintenance/models.py
+++ apps/main/maintenance/models.py
@@ -1,45 +1,47 @@
-from django.db import models
-
 from django.conf import settings
 from django.db import models
 from datetime import datetime  
-from django.dispatch import receiver
-from django.db.models.signals import post_save
 from model_utils.models import TimeStampedModel
-from django.core.validators import MaxValueValidator, MinValueValidator
 
-# Create your models here.
-# fecha, responsable, resultado y observacion. 
-# sin embargo, ahora con la nueva
-# info  que manejamos hoy habria que 
-# indicar a que cosa se le realizo (tanque o planta)
+from apps.main.equipments.models import Equipment
+from apps.main.products.models import Product
 
 class Maintenance(TimeStampedModel):
-
+    #foreign key
     equipment = models.CharField(
         max_length=20,
         null=False
     )
 
-    in_charge = models.TextField(
-        max_length=30,
-        null=False,
-    )
-
-    #lol
-    made_date = models.DateTimeField(
-        blank=True,
-        null=True,
-    )
-
-    observation = models.TextField(
+    activity = models.TextField(
         max_length=600,
         blank=True,
         null=True,
     )
 
-    results = models.TextField(
+    comments = models.TextField(
         max_length=600,
         blank=False,
         null=True,
+    )
+
+    product =  models.ForeignKey(
+        Product, 
+        blank=True, 
+        null=True, 
+        related_name='product',
+        on_delete = models.SET_NULL,
+    )
+
+    product_quantity = models.IntegerField(
+        blank = True,
+        null = True,
+    )
+
+    created_by = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        blank = True,
+        null = True,
+        on_delete = models.SET_NULL,
+        related_name = 'user_creator'
     )
