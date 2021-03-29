df922f9d76850ffb1b0a7ef3c1fba310125cab7d
Pablo Gonzalez <pagoncastro@gmail.com>
2021-03-29 19:47:35 -0400

    Validaciones dinámicas faltantes

--- ong_web/static/assets/js/maintenance/maintenance.js
+++ ong_web/static/assets/js/maintenance/maintenance.js
@@ -6,56 +6,59 @@ document.addEventListener('DOMContentLoaded', function() {
     document.getElementById("product_quantity").addEventListener("change", product_quantity_validation)
 })
 
+// Validación dinámica de la actividad de mantenimiento
 function activity_validation() {
     activity = document.getElementById("activity").value
 
     if (activity == "none" || activity == "") {
         document.getElementById("activity_error").innerHTML = "Introduzca la actividad de mantenimiento a realizar"
-        document.getElementById("activity_error").style.display = "block"
+        document.getElementById("activity_error").style.visibility = "visible"
     }
     else {
-        document.getElementById("activity_error").style.display = "none"
+        document.getElementById("activity_error").style.visibility = "hidden"
     }
 }
 
-
+// Validación dinámica de la descripción de mantenimiento
 function comments_validation() {
     comments = document.getElementById("comments").value
 
     if (comments == "none" || comments == "") {
         document.getElementById("comments_error").innerHTML = "Por favor, introduzca algún comentario respecto al mantenimiento"
-        document.getElementById("comments_error").style.display = "block"
+        document.getElementById("comments_error").style.visibility = "visible"
     }
     else {
-        document.getElementById("comments_error").style.display = "none"
+        document.getElementById("comments_error").style.visibility = "hidden"
     }
 }
 
+// Validación dinámica del producto a usar en el mantenimiento
 function product_name_validation() {
     product_name = document.getElementById("product_name").value
 
     if (product_name == "none") {
         document.getElementById("product_name_error").innerHTML = "Seleccione el producto a usar en el mantenimiento"
-        document.getElementById("product_name_error").style.display = "block"
+        document.getElementById("product_name_error").style.visibility = "visible"
     }
     else {
-        document.getElementById("product_name_error").style.display = "none"
+        document.getElementById("product_name_error").style.visibility = "hidden"
     }
 }
 
+// Validación dinámica de la cantidad de producto a usar
 function product_quantity_validation() {
     product_quantity = document.getElementById("product_quantity").value
 
     if (parseInt(product_quantity) < 1) {
         document.getElementById("product_quantity_error").innerHTML = "Debe introducir una cantidad válida"
-        document.getElementById("product_quantity_error").style.display = "block"
+        document.getElementById("product_quantity_error").style.visibility = "visible"
     }
     else {
-        document.getElementById("product_quantity_error").style.display = "none"
+        document.getElementById("product_quantity_error").style.visibility = "hidden"
     }
 }
 
-
+// Función de validación del form
 function validate() {
     error = false
 
@@ -75,25 +78,25 @@ function validate() {
             // Verificación de cantidad válida
             if (parseInt(product_quantity) < 1) {
                 document.getElementById("product_quantity_error").innerHTML = "Debe introducir una cantidad válida"
-                document.getElementById("product_quantity_error").style.display = "block"
+                document.getElementById("product_quantity_error").style.visibility = "visible"
             }
             // Verificación de disponibilidad
             else if (parseInt(product_quantity) > parseInt(found.product_quantity)) {
                 document.getElementById("product_quantity_error").innerHTML = "La cantidad pedida excede la cantidad disponible"
-                document.getElementById("product_quantity_error").style.display = "block"
+                document.getElementById("product_quantity_error").style.visibility = "visible"
             } else {
-                document.getElementById("product_quantity_error").style.display = "none"
+                document.getElementById("product_quantity_error").style.visibility = "hidden"
             }
 
         } else if (product_quantity && !product_name) {
             // Caso: Se puso cantidad pero no nombre.
             console.log("Entro aqui? Quantity: " + product_quantity + " Name: " + product_name)
             document.getElementById("product_name_error").innerHTML = "Por favor, introduzca el nombre del producto a utilizar"
-            document.getElementById("product_name_error").style.display = "block" 
+            document.getElementById("product_name_error").style.visibility = "visible" 
         } else if (product_name && !product_quantity) {
             // Caso: Se puso nombre pero no cantidad
             document.getElementById("product_quantity_error").innerHTML = "Por favor, introduzca la cantidad de producto a utilizar"
-            document.getElementById("product_quantity_error").style.display = "block" 
+            document.getElementById("product_quantity_error").style.visibility = "visible" 
         }
     }
 
@@ -106,12 +109,12 @@ function validate() {
             // Verificicación de cantidad válida
             if (parseInt(product_quantity) < 1 || product_quantity == "none" || product_quantity == "") {
                 document.getElementById("product_quantity_error").innerHTML = "Debe introducir una cantidad válida"
-                document.getElementById("product_quantity_error").style.display = "block"
+                document.getElementById("product_quantity_error").style.visibility = "visible"
             }
             // Si la cantidad nueva es mayor que la vieja, entonces nueva-vieja < disponible
             else if (parseInt(old_quantity) < parseInt(product_quantity) && parseInt(product_quantity) - parseInt(old_quantity) > found.product_quantity) {     
                 document.getElementById("product_quantity_error").innerHTML = "Cantidad inválida"
-                document.getElementById("product_quantity_error").style.display = "block"
+                document.getElementById("product_quantity_error").style.visibility = "visible"
             }
             
         }
@@ -119,42 +122,36 @@ function validate() {
 
     if (activity == "none" || activity == "") {
         document.getElementById("activity_error").innerHTML = "Introduzca la actividad de mantenimiento a realizar"
-        document.getElementById("activity_error").style.display = "block"
+        document.getElementById("activity_error").style.visibility = "visible"
     }
     else {
-        document.getElementById("activity_error").style.display = "none"
+        document.getElementById("activity_error").style.visibility = "hidden"
     }
 
     if (comments == "none" || comments == "" ) {
         document.getElementById("comments_error").innerHTML = "Por favor, introduzca algún comentario respecto al mantenimiento"
-        document.getElementById("comments_error").style.display = "block"
+        document.getElementById("comments_error").style.visibility = "visible"
     }
     else {
-        document.getElementById("comments_error").style.display = "none"
+        document.getElementById("comments_error").style.visibility = "hidden"
     }
 
     // // Verificacion de uso de proucto
     // if (product_name == "none" || product_name == "") {
     //     document.getElementById("product_name_error").innerHTML = "Por favor, introduzca la cantidad de producto a utilizar"
-    //     document.getElementById("product_name_error").style.display = "block"    
+    //     document.getElementById("product_name_error").style.visibility = "visible"    
     // }
     // else {
-    //     document.getElementById("product_name_error").style.display = "none"
+    //     document.getElementById("product_name_error").style.visibility = "hidden"
     // }
 
-    
+    activity_error = document.getElementById("activity_error").style.visibility
+    comments_error = document.getElementById("comments_error").style.visibility
+    product_quantity_error = document.getElementById("product_quantity_error").style.visibility
+    product_name_error = document.getElementById("product_name_error").style.visibility
 
 
-    
-
-
-    activity_error = document.getElementById("activity_error").style.display
-    comments_error = document.getElementById("comments_error").style.display
-    product_quantity_error = document.getElementById("product_quantity_error").style.display
-    product_name_error = document.getElementById("product_name_error").style.display
-
-
-    if (activity_error == "block" || comments_error == "block" || product_quantity_error == "block" || product_name_error == "block"){
+    if (activity_error == "visible" || comments_error == "visible" || product_quantity_error == "visible" || product_name_error == "visible"){
         error = true
     }
 
---
+++ ong_web/static/assets/js/products/products.js
@@ -0,0 +1,274 @@
+document.addEventListener('DOMContentLoaded', function() {
+    
+    document.getElementById("product_unitary_amount").addEventListener("change", amount_validation)
+    document.getElementById("product_unit").addEventListener("change", unit_validation)
+    
+    // Solo para product_create
+    if (window.location.href.includes("create")) {
+        document.getElementById("product_cod").addEventListener("change", cod_validation)
+        document.getElementById("product_cod").addEventListener("change", quantity_validation)
+        document.getElementById("product_cod").addEventListener("change", amount_validation)
+        document.getElementById("product_cod").addEventListener("change", unit_validation)
+        document.getElementById("product_quantity").addEventListener("change", quantity_validation)
+    }
+
+    // Solo para cupboard_management_update
+    if (window.location.href.includes("update")) {
+        document.getElementById("operation_type").addEventListener("change", quantity_update_validation)
+        document.getElementById("product_quantity").addEventListener("change", quantity_update_validation)
+    }
+})
+
+var unit_found = false;
+
+// Validación dinámica del nombre del producto
+function cod_validation() {
+    product_cod = document.getElementById("product_cod").value
+    operation_type = document.getElementById("operation_type").value
+
+    // Si es introducido un nombre
+    if (product_cod != "" && product_cod != "none") {
+        // Si la operación es un "egreso", verifico si el producto introducido existe
+        if (operation_type == "Egreso") {
+            found = products.find(product => product.product_name == product_cod);
+
+            if (!found) {
+                document.getElementById("product_cod_error").innerHTML = "El producto seleccionado no se encuentra registrado"
+                document.getElementById("product_cod_error").style.visibility = "visible"
+            }
+            else {
+                document.getElementById("product_cod_error").style.visibility = "hidden"
+            }
+        } else {
+            document.getElementById("product_cod_error").style.visibility = "hidden"
+        } 
+    } else {
+        document.getElementById("product_cod_error").innerHTML = "Debe introducir el nombre del producto"
+        document.getElementById("product_cod_error").style.visibility = "visible"
+    }
+}
+
+// Validación dinámica de la cantidad del producto
+function quantity_validation() {
+    product_quantity = parseFloat(document.getElementById("product_quantity").value)
+    operation_type = document.getElementById("operation_type").value
+
+    // Si la cantidad del producto introducida es menor a uno (1)
+    if (product_quantity < 1) {
+        document.getElementById("product_quantity_error").innerHTML = "Debe introducir una cantidad mayor a 0"
+        document.getElementById("product_quantity_error").style.visibility = "visible"
+        return
+    }
+
+    // Si la cantidad del producto introducida es mayor o igual a uno (1)
+    else if (product_quantity >= 1) {
+
+        // Si la cantidad del producto introducida posee decimales
+        if (product_quantity % 1 != 0) {
+            document.getElementById("product_quantity_error").innerHTML = "Debe introducir una cantidad válida"
+            document.getElementById("product_quantity_error").style.visibility = "visible"
+            return
+        }
+        else {
+            document.getElementById("product_quantity_error").style.visibility = "hidden"
+        }
+    }
+    else {
+        document.getElementById("product_quantity_error").style.visibility = "hidden"
+    }
+
+    // Si la operación es un "egreso", verifico la cantidad disponible del producto
+    if (operation_type == "Egreso") {
+        found = products.find(product => product.product_name == product_cod);
+
+        if (found) {
+            if (product_quantity > parseInt(found.product_quantity)) {
+                document.getElementById("product_quantity_error").innerHTML = "La cantidad pedida excede la cantidad disponible"
+                document.getElementById("product_quantity_error").style.visibility = "visible"
+            }
+            else {
+                document.getElementById("product_quantity_error").style.visibility = "hidden"
+            }
+        }
+    }
+}
+
+// Validación dinámica del monto del producto
+function amount_validation() {
+    product_unitary_amount = parseFloat(document.getElementById("product_unitary_amount").value)
+
+    // Si el monto introducido es menor a cero (0)
+    if (product_unitary_amount < 0) {
+        document.getElementById("product_unitary_amount_error").innerHTML = "Debe introducir un precio igual o mayor a 0"
+        document.getElementById("product_unitary_amount_error").style.visibility = "visible"
+    }
+    else {
+        document.getElementById("product_unitary_amount_error").style.visibility = "hidden"
+    }
+}
+
+// Validacion dinámica de las unidades del producto
+function unit_validation() {
+
+    product_cod = document.getElementById("product_cod").value;
+    const found = products.find(product => product.product_name == product_cod);
+
+    if (found) {
+        document.getElementById("product_unit").value = found.product_unit;
+        document.getElementById("product_unit").setAttribute("disabled", 'disabled');
+        document.getElementById("product_unit_error").style.visibility = "hidden"
+        unit_found = true;
+    } else if (!found && document.getElementById("product_unit").disabled) {
+        document.getElementById("product_unit").disabled = false;
+        document.getElementById("product_unit_error").style.visibility = "hidden"
+        unit_found = false;
+    } else {
+        document.getElementById("product_unit_error").style.visibility = "hidden"
+    }
+
+}
+
+
+// Validación estática al momento de enviar el formulario
+function validate() {
+
+    error = false
+
+    // Solo para product_management_create 
+    if (window.location.href.includes("create")) {
+        product_name = document.getElementById("product_cod").value
+    } else if (window.location.href.includes("update")) {
+        product_name = document.getElementById("product_name").value
+    }
+
+    product_unit = document.getElementById("product_unit").value
+    product_quantity = document.getElementById("product_quantity").value
+    product_unitary_amount = document.getElementById("product_unitary_amount").value
+
+    if (document.getElementById("product_unit").disabled) {
+        document.getElementById("product_unit").disabled = false;
+    }
+
+    // Si el nombre del producto es vacío
+    if (product_name == "") {
+        document.getElementById("product_cod_error").innerHTML = "Debe introducir el nombre del producto"
+        document.getElementById("product_cod_error").style.visibility = "visible"
+        error = true
+    } 
+
+    // Si la cantidad del producto es vacía
+    if (product_quantity == "") {
+        document.getElementById("product_quantity_error").innerHTML = "Debe introducir una cantidad mayor a 0"
+        document.getElementById("product_quantity_error").style.visibility = "visible"
+        error = true
+    }
+
+    // Si el monto del producto es vacío
+    if (product_unitary_amount == "") {
+        document.getElementById("product_unitary_amount_error").innerHTML = "Debe introducir un precio igual o mayor a 0"
+        document.getElementById("product_unitary_amount_error").style.visibility = "visible"
+        error = true
+    }
+
+    // Si el nombre de la unidad es vacío
+    if (product_unit == "" || product_unit == "none") {
+        document.getElementById("product_unit_error").innerHTML = "Debe introducir el nombre de la unidad" 
+        document.getElementById("product_unit_error").style.visibility = "visible"
+        error = true
+    }
+
+    product_quantity_error = document.getElementById("product_quantity_error").style.visibility
+    product_unitary_amount_error = document.getElementById("product_unitary_amount_error").style.visibility
+
+    // Solo para product_management_create 
+    if (window.location.href.includes("create")) {
+        product_name_error = document.getElementById("product_cod_error").style.visibility
+
+        if (product_name_error == "visible" || product_quantity_error == "visible" || product_unitary_amount_error == "visible"){
+            error = true
+        }
+    }
+
+    // Solo para cupboard_management_update
+    if (window.location.href.includes("update")) {
+        if (product_quantity_error == "visible" || product_unitary_amount_error == "visible"){
+            error = true
+        }
+    }
+
+    // Si existe un error en el formulario, no se envía
+    if (error) {
+        if (unit_found) {
+            document.getElementById("product_unit").setAttribute("disabled", 'disabled');
+            unit_found = false;
+        }
+        return false
+    }
+    else {
+        return true
+    }
+}
+
+// Validación dinámica de la actualización de la cantidad de productos
+function quantity_update_validation() {
+    product_quantity_update = parseFloat(document.getElementById("product_quantity").value)
+    operation_type_update = document.getElementById("operation_type").value
+
+    product = products.find(i => i.product_name == product_name);
+    current_quantity = parseInt(product.product_quantity)
+
+    // Si la cantidad de productos introducido es menor a uno (1)
+    if (product_quantity_update < 1) {
+        document.getElementById("product_quantity_error").innerHTML = "Debe introducir una cantidad mayor a 0"
+        document.getElementById("product_quantity_error").style.visibility = "visible"
+        return
+    }
+
+    // Si la cantidad de productos introducida es mayor o igual a uno (1)
+    else if (product_quantity_update >= 1) {
+
+        // Si la cantidad de productos introducida posee decimales
+        if (product_quantity_update % 1 != 0) {
+            document.getElementById("product_quantity_error").innerHTML = "Debe introducir una cantidad de productos válida"
+            document.getElementById("product_quantity_error").style.visibility = "visible"
+            return
+        }
+        else {
+            document.getElementById("product_quantity_error").style.visibility = "hidden"
+        }
+    }
+    else {
+        document.getElementById("product_quantity_error").style.visibility = "hidden"
+    }
+
+    original_operation_quantity = parseInt(original_operation_quantity)
+    original_operation_type = original_operation_type
+
+    var base_quantity
+
+    // Dependiendo del tipo de operación, se hace una regresión acorde para tener la cantidad del producto disponible
+    if (original_operation_type == "Ingreso") {
+        base_quantity = current_quantity - original_operation_quantity
+    }
+    else {
+        base_quantity = current_quantity + original_operation_quantity
+    }
+
+    // Si el nuevo tipo de operación es "ingreso"
+    if (operation_type_update == "Ingreso") {
+
+        // Si, debido a la nueva cantidad ingresada, la cantidad del producto es menor a cero (0)
+        if (base_quantity + product_quantity_update < 0) {
+            document.getElementById("product_quantity_error").innerHTML = "Operación inválida"
+            document.getElementById("product_quantity_error").style.visibility = "visible"
+        }
+    }
+    // Si el nuevo tipo de operación es "egreso"
+    else {
+        // Si, debido a la nueva cantidad egresada, la cantidad del producto es menor a cero (0) 
+        if (base_quantity - product_quantity_update < 0) {
+            document.getElementById("product_quantity_error").innerHTML = "La cantidad pedida excede la cantidad disponible"
+            document.getElementById("product_quantity_error").style.visibility = "visible"
+        }
+    }
+}
