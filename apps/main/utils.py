723ceca1ae68946b07c4cad54b4c42325cd9d5e8
Pedro Maldonado <pjmg96@gmail.com>
2021-03-30 19:41:15 -0400

    Agregada funcion para obtener path completo de imagen

--- apps/main/utils.py
+++ apps/main/utils.py
@@ -3,18 +3,31 @@ from django.http import HttpResponse
 from django.template.loader import get_template
 from io import BytesIO
 from xhtml2pdf import pisa
+from django import template
+from django.contrib.staticfiles import finders
 
+import os.path
 import requests
+
+
 
 def get_exchange_rate():
     r = requests.get('https://s3.amazonaws.com/dolartoday/data.json')
     return r.json()['USD']['sicad2']
 
+#TODO validar url en linux y luego ver como se hara en Heroku
+def fetch_resources(uri, rel):
+	local = os.path.join(settings.BASE_DIR, 'ong_web')
+	path = os.path.join(local, uri.replace(settings.STATIC_URL, ""))
+	return path
+
 def render_pdf_view(template_src, context_dict={}):
 	template = get_template(template_src)
 	html  = template.render(context_dict)
 	result = BytesIO()
-	pdf = pisa.pisaDocument(BytesIO(html.encode("ISO-8859-1")), result)
+	pdf = pisa.pisaDocument(BytesIO(html.encode("ISO-8859-1")), result, link_callback=fetch_resources)
 	if not pdf.err:
 		return HttpResponse(result.getvalue(), content_type='application/pdf')
 	return None
+
+	
