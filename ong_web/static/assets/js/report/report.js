6cc2337d68de2ce16e493917ad29237aeebe490b
Javier <javivasv@hotmail.com>
2021-04-01 13:43:51 -0400

    Validaciones de solicitud de reporte

--- ong_web/static/assets/js/report/report.js
+++ ong_web/static/assets/js/report/report.js
@@ -1,104 +1,88 @@
-/*
-document.addEventListener('DOMContentLoaded', function() {
-})
-*/
+document.getElementById("from_date").addEventListener("change", check_from_date)
+document.getElementById("to_date").addEventListener("change", check_to_date)
 
-document.getElementById("prueba").addEventListener("click", prueba)
-
-function prueba() {
-    
-    from_date = document.getElementById("from_date").value.split("-")
-    from_day = parseInt(from_date[2])
-    from_month = parseInt(from_date[1])
-    from_year = parseInt(from_date[0])
-
-    to_date = document.getElementById("to_date").value.split("-")
-    to_day = parseInt(to_date[2])
-    to_month = parseInt(to_date[1])
-    to_year = parseInt(to_date[0])
-
+// Validación dinámica de la fecha inicial
+function check_from_date() {
+    from = new Date(document.getElementById("from_date").value);
+    from_date = new Date(from.getFullYear(), from.getMonth(), from.getDate() + 1)
     today = new Date();
-    day = parseInt(String(today.getDate()).padStart(2, '0'));
-    month = parseInt(String(today.getMonth() + 1).padStart(2, '0'));
-    year = today.getFullYear();
 
-    
-    console.log("from day: ", from_day)
-    console.log("to day: ", to_day)
-    console.log("day: ", day)
-    console.log("from month: ", from_month)
-    console.log("to month: ", to_month)
-    console.log("month: ", month)
-    console.log("from year: ", from_year)
-    console.log("to year: ", to_year)
-    console.log("year: ", year)
-    
-    // Validación del campo de fecha inicial
-    if (from_year <= year) {
-        if (from_month <= month) {
-            if (from_day <= day) {
-                document.getElementById("from_error").style.visibility = "hidden"
-            }
-            else {
-                document.getElementById("from_error").innerHTML = "Debe introducir una fecha inicial válida"
-                document.getElementById("from_error").style.visibility = "visible"
-            }
-        }
-        else {
-            document.getElementById("from_error").innerHTML = "Debe introducir una fecha inicial válida"
-            document.getElementById("from_error").style.visibility = "visible"
-        }
-    }
-    else {
+    if (from_date > today) {
         document.getElementById("from_error").innerHTML = "Debe introducir una fecha inicial válida"
         document.getElementById("from_error").style.visibility = "visible"
     }
+    else {
+        document.getElementById("from_error").style.visibility = "hidden"
+    }
 
-    // Validación del campo de fecha final
-    if (to_year <= year) {
-        if (to_month <= month) {
-            if (to_day <= day) {
-                document.getElementById("to_error").style.visibility = "hidden"
-            }
-            else {
-                document.getElementById("to_error").innerHTML = "Debe introducir una fecha final válida"
-                document.getElementById("to_error").style.visibility = "visible"
-            }
-        }
-        else {
-            document.getElementById("to_error").innerHTML = "Debe introducir una fecha final válida"
-            document.getElementById("to_error").style.visibility = "visible"
-        }
+    to = new Date(document.getElementById("to_date").value);
+    to_date = new Date(to.getFullYear(), to.getMonth(), to.getDate() + 1)
+
+    if (from_date > to_date) {
+        document.getElementById("interval_error").innerHTML = "Debe introducir un intervalo de tiempo válido"
+        document.getElementById("interval_error").style.visibility = "visible"
     }
     else {
+        document.getElementById("interval_error").style.visibility = "hidden"
+    }
+}
+
+// Validación dinámica de la fecha final
+function check_to_date() {
+    to = new Date(document.getElementById("to_date").value);
+    to_date = new Date(to.getFullYear(), to.getMonth(), to.getDate() + 1)
+    today = new Date();
+
+    if (to_date > today) {
         document.getElementById("to_error").innerHTML = "Debe introducir una fecha final válida"
         document.getElementById("to_error").style.visibility = "visible"
     }
+    else {
+        document.getElementById("to_error").style.visibility = "hidden"
+    }
 
-    // Validación del campo de fecha inicial con respecto al campo de fecha final
-    if (from_year <= to_year) {
-        if (from_month <= to_month) {
-            if (from_day <= to_day) {
-                document.getElementById("date_error").style.visibility = "hidden"
-            }
-            else {
-                document.getElementById("date_error").innerHTML = "Debe introducir un intervalo de tiempo válido"
-                document.getElementById("date_error").style.visibility = "visible"
-            }
-        }
-        else {
-            document.getElementById("date_error").innerHTML = "Debe introducir un intervalo de tiempo válido"
-            document.getElementById("date_error").style.visibility = "visible"
-        }
+    from = new Date(document.getElementById("from_date").value);
+    from_date = new Date(from.getFullYear(), from.getMonth(), from.getDate() + 1)
+
+    if (from_date > to_date) {
+        document.getElementById("interval_error").innerHTML = "Debe introducir un intervalo de tiempo válido"
+        document.getElementById("interval_error").style.visibility = "visible"
     }
     else {
-        document.getElementById("date_error").innerHTML = "Debe introducir un intervalo de tiempo válido"
-        document.getElementById("date_error").style.visibility = "visible"
+        document.getElementById("interval_error").style.visibility = "hidden"
+    }
+}
+
+// Validación estática al momento de enviar el formulario
+function validate() {
+    error = false
+
+    from = document.getElementById("from_date").value;
+    to = document.getElementById("to_date").value;
+
+    if (from == "") {
+        document.getElementById("from_error").innerHTML = "Debe introducir una fecha inicial"
+        document.getElementById("from_error").style.visibility = "visible"
+    }
+    else {
+        document.getElementById("from_error").style.visibility = "hidden"
     }
 
+    if (to == "") {
+        document.getElementById("to_error").innerHTML = "Debe introducir una fecha final"
+        document.getElementById("to_error").style.visibility = "visible"
+    }
+    else {
+        document.getElementById("to_error").style.visibility = "hidden"
+    }
 
+    from_error = document.getElementById("from_error").style.visibility
+    to_error = document.getElementById("to_error").style.visibility
+    interval_error = document.getElementById("interval_error").style.visibility
 
+    if (from_error == "visible" || to_error == "visible" || interval_error == "visible"){
+        error = true
+    }
 
-
-
+    return (!error)
 }
